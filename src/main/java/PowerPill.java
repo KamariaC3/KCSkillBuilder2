820c30d6269c90af50eba3eeff5a2ce1b4d083fa
kcureton23 <kcureton23@students.desu.edu>
2024-02-09 16:19:16 -0500

    SkillBUilder2

--- src/main/java/PowerPill.java
+++ src/main/java/PowerPill.java
@@ -10,29 +10,53 @@
 public class PowerPill
 {
     // TODO - replace this line with instruction from step 1
-
+    public static int DEFAULT_POWER = 10;
     // instance variables
+     int power;
+     String name;
     // TODO - replace this line with instruction from step 2
     // TODO - replace this line with instruction from step 3
 
     // constructors
 
     // TODO - replace this line with instruction from step 4
+    public PowerPill(String name){
+        power = DEFAULT_POWER;
+        this.name = name;
+    }
 
     // TODO - replace this line with instruction from step 5
-
-
+    public PowerPill(String name, int power) {
+        this.power = power;
+        this.name = name;
+    }
     // accessor methods
 
-    // TODO - replace this line with instruction from step 6
+    public int getPower() {
+        return power;
+    }
 
+    public String getName() {
+         return name;
+    }
+
+    public void setPower(int power) {
+         this.power = power;
+    }
+
+    public void setName(String name) {
+         this.name = name;
+    }
 
     // mutator methods
 
     // TODO - replace this line with instruction from step 7
 
     // toString method
-
+    public String toString(){
+        return "PowerPill " + name + " = " + power;
+        //PowerPill <name> = <power>
+    }
     // TODO - replace this line with instruction from step 8
 
 }
